98e277069a78a64166514b52b884e3c37f9fa990
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-05-04 00:30:27 +0330

    Implement domain management UI with table, search, and filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,40 @@
+import { PlusOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+
+import DomainTable from '@/components/domain-table'
+import SortFilter from '@/components/sort-filter'
+import { useGetDomainsQuery } from '@/services/domain-api'
+
+import SearchInput from './components/search-input'
+
 export default function App() {
-  return <h1>Hello Workd</h1>
+  const { data: domains, isError, isLoading } = useGetDomainsQuery()
+
+  if (isError) {
+    return <p>Error happened!</p>
+  }
+
+  return (
+    <main className="container mx-auto mt-5 space-y-8 max-md:px-2">
+      <h1 className="text-4xl font-bold">Domains</h1>
+
+      <section className="flex items-center justify-between gap-2 max-sm:flex-col">
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          size="large"
+          className="max-sm:w-full"
+        >
+          Add Domain
+        </Button>
+
+        <div className="flex items-center gap-3 lg:gap-6">
+          <SortFilter />
+          <SearchInput />
+        </div>
+      </section>
+
+      <DomainTable domains={domains ?? []} isLoading={isLoading} />
+    </main>
+  )
 }
