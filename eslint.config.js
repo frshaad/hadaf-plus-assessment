dfd84ddfa8426a5001c5ee848223427402fa05ee
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-05-03 11:33:51 +0330

    Configure linter and formatter
    - add prettier config
    - update eslint config to sort imports
    - lint and format all project files

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -25,4 +27,14 @@ export default tseslint.config(
       ],
     },
   },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  prettierConfig,
 )
