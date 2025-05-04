53a58388106504227ae5ce5e19b2e5fdc20fbd30
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-05-04 20:01:53 +0330

    add edit domain functionality with a custom hook

--- src/components/domain-table/index.tsx
+++ src/components/domain-table/index.tsx
@@ -1,37 +1,41 @@
 import { Table } from 'antd'
 
+import { useDeleteDomainModal } from '@/hooks/use-delete-domain-modal'
+import { useDomainColumns } from '@/hooks/use-domain-columns'
+import { useEditDomain } from '@/hooks/use-edit-domain'
 import type { Domain } from '@/types'
 
-import { useDeleteDomainModal } from '../../hooks/use-delete-domain-modal'
-import { useDomainColumns } from '../../hooks/use-domain-columns'
+import DomainDrawer from '../domain-drawer'
 
 export type DomainTableProps = {
   domains: Domain[]
   isLoading: boolean
-  onEditDomain: (domain: Domain) => void
-  onToggleVerify: (domain: Domain) => void
-  onToggleActivate: (domain: Domain) => void
 }
 
-export default function DomainTable({
-  domains,
-  isLoading,
-  onEditDomain,
-  onToggleVerify,
-  onToggleActivate,
-}: DomainTableProps) {
+export default function DomainTable({ domains, isLoading }: DomainTableProps) {
   const { showDeleteModal, DeleteModal } = useDeleteDomainModal()
+  const {
+    showDrawer: showEditDrawer,
+    openDrawer,
+    closeDrawer,
+    onSubmit,
+    currentDomain,
+    contextHolder,
+    loading: isEditing,
+  } = useEditDomain()
 
   const columns = useDomainColumns({
-    onEdit: onEditDomain,
+    onEdit: showEditDrawer,
     onDelete: (domain: Domain) => {
       showDeleteModal(domain)
     },
-    onToggleVerify,
-    onToggleActivate,
+    onToggleVerify: (domain) => console.log('verify', domain),
+    onToggleActivate: (domain) => console.log('activate', domain),
   })
+
   return (
     <>
+      {contextHolder}
       <Table
         rowKey="_id"
         columns={columns}
@@ -41,6 +45,14 @@ export default function DomainTable({
         pagination={{ pageSize: 10 }}
       />
       <DeleteModal />
+      <DomainDrawer
+        open={openDrawer}
+        onClose={closeDrawer}
+        onSubmit={onSubmit}
+        loading={isEditing}
+        defaultValue={currentDomain?.domain}
+        type="update"
+      />
     </>
   )
 }
---
+++ src/hooks/use-edit-domain.ts
@@ -0,0 +1,59 @@
+import { message } from 'antd'
+import { useState } from 'react'
+
+import { useUpdateDomainMutation } from '@/services/domain-api'
+import { Domain } from '@/types'
+
+export function useEditDomain() {
+  const [openDrawer, setOpenDrawer] = useState(false)
+  const [currentDomain, setCurrentDomain] = useState<Domain | null>(null)
+
+  const [updateDomain, { isLoading }] = useUpdateDomainMutation()
+  const [messageApi, contextHolder] = message.useMessage()
+
+  const showDrawer = (domain: Domain) => {
+    setCurrentDomain(domain)
+    setOpenDrawer(true)
+  }
+
+  const closeDrawer = () => {
+    setCurrentDomain(null)
+    setOpenDrawer(false)
+  }
+
+  const onSubmit = async (updatedDomain: string) => {
+    if (!currentDomain) return
+
+    try {
+      messageApi.open({
+        type: 'loading',
+        content: 'Updating domain...',
+        duration: 0,
+      })
+
+      await updateDomain({
+        id: currentDomain.id,
+        data: { domain: updatedDomain },
+      }).unwrap()
+
+      messageApi.destroy()
+      messageApi.success('Domain updated')
+
+      closeDrawer()
+    } catch (error) {
+      console.error(error)
+      messageApi.destroy()
+      messageApi.error('Error happened!')
+    }
+  }
+
+  return {
+    openDrawer,
+    showDrawer,
+    closeDrawer,
+    onSubmit,
+    loading: isLoading,
+    contextHolder,
+    currentDomain,
+  }
+}
