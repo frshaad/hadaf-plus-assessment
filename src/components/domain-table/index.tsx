a3c5e5c165c3c42146b1f0c5b2868e037609498d
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-05-04 17:18:04 +0330

    Refactor domain table with enhanced action controls

--- src/components/domain-table/index.tsx
+++ src/components/domain-table/index.tsx
@@ -2,25 +2,38 @@ import { Table } from 'antd'
 
 import type { Domain } from '@/types'
 
-import { useDeleteDomainModal } from './use-delete-domain-modal'
-import { useDomainColumns } from './use-domain-columns'
+import { useDeleteDomainModal } from '../../hooks/use-delete-domain-modal'
+import { useDomainColumns } from '../../hooks/use-domain-columns'
 
 export type DomainTableProps = {
   domains: Domain[]
   isLoading: boolean
+  onEditDomain: (domain: Domain) => void
+  onToggleVerify: (domain: Domain) => void
+  onToggleActivate: (domain: Domain) => void
 }
 
-export default function DomainTable({ domains, isLoading }: DomainTableProps) {
+export default function DomainTable({
+  domains,
+  isLoading,
+  onEditDomain,
+  onToggleVerify,
+  onToggleActivate,
+}: DomainTableProps) {
   const { showDeleteModal, DeleteModal } = useDeleteDomainModal()
-  const columns = useDomainColumns({
-    onDelete: showDeleteModal,
-    onEdit: () => console.log('Edit'),
-  })
 
+  const columns = useDomainColumns({
+    onEdit: onEditDomain,
+    onDelete: (domain: Domain) => {
+      showDeleteModal(domain)
+    },
+    onToggleVerify,
+    onToggleActivate,
+  })
   return (
     <>
       <Table
-        rowKey="id"
+        rowKey="_id"
         columns={columns}
         dataSource={domains}
         loading={isLoading}
---
+++ src/hooks/use-domain-columns.tsx
@@ -0,0 +1,108 @@
+import { MoreOutlined } from '@ant-design/icons'
+import { Button, Dropdown, Menu } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+
+import { DomainTableProps } from '@/components/domain-table'
+import type { Domain, VerificationStatus } from '@/types'
+
+type UseDomainColumnsProps = {
+  onEdit: (domain: Domain) => void
+  onDelete: (domain: Domain) => void
+  onToggleVerify: (domain: Domain) => void
+  onToggleActivate: (domain: Domain) => void
+}
+
+export function useDomainColumns({
+  onDelete,
+  onEdit,
+  onToggleActivate,
+  onToggleVerify,
+}: UseDomainColumnsProps) {
+  const columns: ColumnsType<DomainTableProps['domains'][number]> = [
+    {
+      title: 'Domain',
+      dataIndex: 'domain',
+      key: 'domain',
+    },
+    {
+      title: 'Verification Status',
+      dataIndex: 'status',
+      key: 'status',
+      render: (status: VerificationStatus) =>
+        status == 'verified' ? (
+          <span className="text-[#52c41a]">Verified</span>
+        ) : (
+          <span className="text-[#eb2f96]">Not Verified</span>
+        ),
+    },
+    {
+      title: 'Active Status',
+      dataIndex: 'isActive',
+      key: 'isActive',
+      render: (isActive: boolean) =>
+        isActive ? (
+          <span className="text-[#52c41a]">Active</span>
+        ) : (
+          <span className="text-[#eb2f96]">Not Active</span>
+        ),
+    },
+    {
+      title: 'Date Created',
+      dataIndex: 'createdDate',
+      key: 'createdDate',
+      render: (timestamp: number) =>
+        new Intl.DateTimeFormat('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        }).format(new Date(timestamp * 1000)),
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      render: (_text, record) => {
+        const verifyActionText =
+          record.status === 'verified' ? 'Invalidate' : 'Verify'
+        const activateActionText = record.isActive ? 'Deactivate' : 'Activate'
+
+        const menu = (
+          <Menu
+            items={[
+              {
+                key: 'edit',
+                label: 'Edit Domain',
+                onClick: () => onEdit(record),
+              },
+              {
+                key: 'verify',
+                label: verifyActionText,
+                onClick: () => onToggleVerify(record),
+              },
+              {
+                key: 'activate',
+                label: activateActionText,
+                onClick: () => onToggleActivate(record),
+              },
+              {
+                type: 'divider',
+              },
+              {
+                key: 'delete',
+                label: <span className="text-red-500">Delete</span>,
+                onClick: () => onDelete(record),
+              },
+            ]}
+          />
+        )
+
+        return (
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button type="text" icon={<MoreOutlined />} />
+          </Dropdown>
+        )
+      },
+    },
+  ]
+
+  return columns
+}
